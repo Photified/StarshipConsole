72e37d0a1e80cf0e256ebbbceef9d973d7c15700
Photified <justinmainphoto@gmail.com>
2026-01-12 22:32:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,40 @@
-const cacheName = 'starship-v1';
-const assets = ['./', './index.html', 'https://unpkg.com/three@0.160.0/build/three.module.js'];
+const cacheName = 'starship-console-v1';
+const assets = [
+  './',
+  './index.html',
+  './stars.json',
+  './manifest.json',
+  './icon-512.png',
+  'https://unpkg.com/three@0.160.0/build/three.module.js'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Install the Service Worker and cache all files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('LCARS Memory Bank: Caching System Files');
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch files from cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
